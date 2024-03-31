1d2fc65d8dbc77069257b74d439daa01793c508b
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2024-03-31 17:51:19 -0300

    Exemplo - Aula 2

---
+++ Aula02/Exemplo1/src/Main.java
@@ -0,0 +1,22 @@
+// Exemplo Aula 02 FOR/WHILE/CASE
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 2;
+        int repetir = 0; do{
+
+            System.out.println("Caso escolha....");
+
+            switch (x) {
+                case 1:
+                    System.out.println("Ótima escolha!"); break;
+                case 2:
+                    System.out.println("O valor da blusa é R$ 2,00"); break;
+                case 3:
+                    System.out.println("Falar com atendente"); break;
+                default: System.out.println("Inválido"); break;
+            }
+            System.out.print("Para repetir, digite 0"); }
+        while(repetir ==0);
+    }
+}
