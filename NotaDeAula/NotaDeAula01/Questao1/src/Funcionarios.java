f30c8dedf2cd8c6e0a5580979f8a8a21bd7713ff
Nícollas Garibaldi <143560192+nicollasgaribaldi@users.noreply.github.com>
2024-03-31 22:14:41 -0300

    Correção de commits

---
+++ NotaDeAula/NotaDeAula01/Questao1/src/Funcionarios.java
@@ -0,0 +1,16 @@
+public class Funcionarios {
+
+    int matricula;
+    String nome;
+    double salario_bruto;
+
+    public void calc_deducao() {
+
+        double deducao = salario_bruto * 0.15;
+        double salario_liquido = salario_bruto - deducao;
+
+        System.out.println("Quanto sera deduzido do INSS (15%): " + deducao);
+        System.out.println("Salario Liquido apos dedução: "+ salario_liquido);
+        return;
+    }
+}
---
+++ NotaDeAula/NotaDeAula01/Questao1/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String r;
+
+        Funcionarios f = new Funcionarios();
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println();
+            System.out.println("------ FOLHA DE PAGAMENTO ------");
+            System.out.println();
+
+            System.out.print("Digite o nome do funcionario: ");
+            f.nome = sc.next();
+
+            System.out.print("Digite a matricula do funcionario: ");
+            f.matricula = sc.nextInt();
+
+            System.out.print("Digite o salario bruto do funcionario: ");
+            f.salario_bruto = sc.nextDouble();
+
+            System.out.println();
+            System.out.println("Exibindo os dados");
+            System.out.println();
+            System.out.println("Nome do Funcionario: " + f.nome);
+            System.out.println("Matricula: " + f.matricula);
+            System.out.println("Salario Bruto: " + f.salario_bruto);
+            f.calc_deducao();
+
+            System.out.println();
+            System.out.print("Para usar o programa denovo, digite 'S'; Se não quiser executar, digite 'N': ");
+            r = sc.next();
+
+            if (r.equalsIgnoreCase("N")){
+                System.out.println("Obrigado por usar o programa");
+                break;
+            }
+        } while (r.equalsIgnoreCase("S"));
+    }
+}
